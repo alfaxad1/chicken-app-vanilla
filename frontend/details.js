b93796bedd21c9c27e4ca3e3be93ed9c43536278
alfaxad1 <alfaxadder@gmail.com>
2025-03-19 17:04:01 +0300

    form containers

--- frontend/details.js
+++ frontend/details.js
@@ -1,3 +1,66 @@
+const purchaseContainer = document.getElementById("purchase-container");
+purchaseContainer.style.display = "none";
+
+const expenseContainer = document.getElementById("expense-container");
+expenseContainer.style.display = "none";
+
+const chickenLossContainer = document.getElementById("chicken-loss-container");
+chickenLossContainer.style.display = "none";
+
+//chicken loss container start
+const createChickenLoss = () => {
+  chickenLossContainer.style.display = "flex";
+  document.body.style.overflow = "hidden";
+};
+
+const closeChickenLossForm = () => {
+  chickenLossContainer.style.display = "none";
+  document.body.style.overflow = "auto";
+};
+
+chickenLossContainer.addEventListener("click", (e) => {
+  if (e.target === chickenLossContainer) {
+    closeChickenLossForm();
+  }
+});
+//chicken loss container end
+
+//purchases container start
+const createPurchase = () => {
+  purchaseContainer.style.display = "flex";
+  document.body.style.overflow = "hidden";
+};
+
+const closeForm = () => {
+  purchaseContainer.style.display = "none";
+  document.body.style.overflow = "auto";
+};
+
+purchaseContainer.addEventListener("click", (e) => {
+  if (e.target === purchaseContainer) {
+    closeForm();
+  }
+});
+//purchases container end
+
+//expense container start
+const createExpense = () => {
+  expenseContainer.style.display = "flex";
+  document.body.style.overflow = "hidden";
+};
+
+const closeExpenseForm = () => {
+  expenseContainer.style.display = "none";
+  document.body.style.overflow = "auto";
+};
+
+expenseContainer.addEventListener("click", (e) => {
+  if (e.target === expenseContainer) {
+    closeForm();
+  }
+});
+//expense container end
+
 document.addEventListener("DOMContentLoaded", function () {
   const url = "http://localhost:3000";
   const id = localStorage.getItem("ID");
